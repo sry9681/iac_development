148ce3ba844e98847bcbfa6d7e49f5ad533a757e
Sam Yeager <syeager@evtcorp.com>
2025-03-25 15:52:09 -0500

    intial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "test-bucket-${random_string.bucket_suffix.result}"
+}
+
+resource "random_string" "bucket_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+} 
